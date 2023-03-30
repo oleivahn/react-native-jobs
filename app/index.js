f126759db6b66088c7b96dfdfd9ac71382a008b2
Omar Leiva <oleivahn@gmail.com>
2023-03-30 01:24:06 -0400

    First commit - Scafold

---
+++ app/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES, FONTS } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+// THIS IS THE HOME SCREEN - THE MAIN SCREEN
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      {/* Top header menu */}
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      {/* Main content - THE BODY OF THE MAIN PAGE*/}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
